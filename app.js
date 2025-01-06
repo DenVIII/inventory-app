0f94c1d2d18c599103fdc51230b736dc904147db
Denis <Denis226@gmail.com>
2025-01-06 18:16:30 +0300

    Separete styles from index.ejs in styles.css

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const homeRouter = require("./routes/homeRoute");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 app.use("/", homeRouter);
 
 app.listen(PORT, () => console.log(`Server is running at: localhost:${PORT}`));
