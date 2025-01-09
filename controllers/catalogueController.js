018923621942ac3b9563c56803511282a3717c74
Denis <Denis226@gmail.com>
2025-01-09 16:47:42 +0300

    Added support for rendering grocery cards from db
    In this commit:
    - Deleted placeholder cards from the catalogue
    - Grocery cards are now rendered based on data from the database
    
    New file:
    - `views/grocery_card.ejs` - template for the grocery card
    
    Modified `controllers/catalogueController.js`:
    - The `getCataloguePage` function now fetches grocery data from the database and passes it to the response
    
    Modified `db/queries.js`:
    - Added the `getAllCatalogue` function, which makes a query to the database to retrieve all groceries
    
    Modified `views/catalogue.ejs`:
    - Deleted placeholder grocery cards
    - Grocery cards are now moved to a separate file, `grocery_card.ejs`

--- controllers/catalogueController.js
+++ controllers/catalogueController.js
@@ -2,8 +2,9 @@ const db = require("../db/queries");
 
 async function getCataloguePage(req, res) {
   const categories = await db.getAllCategories();
+  const groceries = await db.getAllCatalogue();
 
-  res.render("catalogue", { categories });
+  res.render("catalogue", { categories, groceries });
 }
 
 module.exports = {
--- db/queries.js
+++ db/queries.js
@@ -5,6 +5,31 @@ async function getAllCategories() {
   return rows;
 }
 
+async function getAllCatalogue() {
+  const { rows } = await pool.query(
+    `
+      SELECT name, quantity, measurment, price_rub, category_name, brand_name 
+      FROM groceries 
+      JOIN category ON groceries.category_id = category.id
+      JOIN brand_grocery ON brand_grocery.grocery_id = groceries.id
+      JOIN brand ON brand_grocery.brand_id = brand.id
+      ORDER BY category_name, name;
+    `
+  );
+  return rows;
+}
+
+/* async function getCatalogueBySearchParameters(queryParams) {
+  
+  if (!queryParams) {
+    return;
+  } 
+  const queryText = ''
+
+  const { rows } = await pool.query("SELECT $1 FROM");
+} */
+
 module.exports = {
   getAllCategories,
+  getAllCatalogue,
 };
