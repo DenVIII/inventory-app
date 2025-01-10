a5feea82aeca29fdf69b2f1e3b177a395f756915
Denis <Denis226@gmail.com>
2025-01-10 13:48:40 +0300

    In this commit: - Category buttons in the navbar now display groceries associated with that category. - Brand and category anchors are now functional and should display groceries associated with them.
    Modified 'controllers/catalogueController.js':
    - The 'getCataloguePage' function now calls the appropriate query function based on whether query parameters exist or not.
    
    Modified 'db/queries.js':
    - Created the 'getCatalogueBySearchQuery' function, which searches the database using the passed query.
    - Implemented the 'createDatabaseQueryFromRequest' function, which takes a request query object and returns a string with parameters for the WHERE clause.
    
    Modified 'views/grocery_card.ejs':
    - Added the correct links to the 'grocery-brand' and 'grocery-category' anchors.
    
    Modified 'views/navbar.ejs':
    - Added the correct links to the 'grocery-category' anchors.

--- controllers/catalogueController.js
+++ controllers/catalogueController.js
@@ -2,7 +2,14 @@ const db = require("../db/queries");
 
 async function getCataloguePage(req, res) {
   const categories = await db.getAllCategories();
-  const groceries = await db.getAllCatalogue();
+
+  let groceries;
+  console.log(req.query);
+  if (Object.keys(req.query).length === 0) {
+    groceries = await db.getAllCatalogue();
+  } else {
+    groceries = await db.getCatalogueBySearchQuery(req.query);
+  }
 
   res.render("catalogue", { categories, groceries });
 }
--- db/queries.js
+++ db/queries.js
@@ -19,17 +19,42 @@ async function getAllCatalogue() {
   return rows;
 }
 
-/* async function getCatalogueBySearchParameters(queryParams) {
-  
-  if (!queryParams) {
-    return;
-  } 
-  const queryText = ''
+async function getCatalogueBySearchQuery(queryParams) {
+  const queryText = `
+      SELECT name, quantity, measurment, price_rub, category_name, brand_name 
+      FROM groceries 
+      JOIN category ON groceries.category_id = category.id
+      JOIN brand_grocery ON brand_grocery.grocery_id = groceries.id
+      JOIN brand ON brand_grocery.brand_id = brand.id
+      WHERE ${createDatabaseQueryFromRequest(queryParams)}
+      ORDER BY category_name, name;
+  `;
+  console.log(queryText);
+  const { rows } = await pool.query(queryText);
 
-  const { rows } = await pool.query("SELECT $1 FROM");
-} */
+  return rows;
+}
+
+function createDatabaseQueryFromRequest(requestQueries) {
+  let result = "";
+  const keys = Object.keys(requestQueries); // Создаем массив ключей-столбцов по которым мы будем фильтровать значения
+
+  keys.forEach((key) => {
+    if (result != "") result += " AND "; // Проверяем был ли уже добавлен параметр запроса и добавляем AND в конец строки, если результат не пустой
+
+    const queryValue = requestQueries[key];
+    if (Array.isArray(queryValue)) {
+      result += `LOWER(${key}) IN (\'${queryValue.join("','")}\')`; // Обрабатываем массив запросов по одному столбцу
+    } else {
+      result += `LOWER(${key}) = LOWER(\'${queryValue}\')`;
+    }
+  });
+
+  return result;
+}
 
 module.exports = {
   getAllCategories,
   getAllCatalogue,
+  getCatalogueBySearchQuery,
 };
